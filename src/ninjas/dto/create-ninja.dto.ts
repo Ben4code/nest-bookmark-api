aa783be07086ae36841cc453cb19d0ed42866f37
Christoher Obioha <110138213+krys93@users.noreply.github.com>
2023-05-17 06:12:18 -0400

    completed ninja crud with pipe and guard

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,9 +1,9 @@
-import { MinLength, isEnum } from 'class-validator';
+import { MinLength, IsEnum } from 'class-validator';
 
 export class CreateNinjaDto {
   @MinLength(3)
   name: string;
 
-  @isEnum(['Katana' | 'shuruken'], { message: 'Use correct weapon!' })
+  @IsEnum(['Katana', 'shuruken'], { message: 'Use correct weapon!' })
   weapon: 'Katana' | 'shuruken';
 }
